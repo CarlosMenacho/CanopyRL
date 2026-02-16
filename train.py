aaaa27bc5a076dbbadc7fc2865b56d1d0ccd4226
Carlos Menacho <menachocarlos5@gmail.com>
2026-02-16 01:35:13 -0400

    Improve training config

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 import torch
 import numpy as np
 import logging
@@ -8,8 +8,7 @@ logger = logging.getLogger(__name__)
 
 @hydra.main(version_base="1.2", config_path="config", config_name="config")
 def main(cfg: DictConfig) -> None:
-    logger.info("Config: ")
-    logger.info(cfg)
+    logger.info(f"Config: \n{OmegaConf.to_yaml(cfg)}")
 
 if __name__ == "__main__":
     main()
